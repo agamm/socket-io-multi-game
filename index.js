7e7da2192e2a6fe0f2bd1c9db6ee45aa0266e0f3
Agam More <agam@agam.me>
2022-12-27 19:09:24 +0200

    send all players on start

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ let players = {};
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+  io.emit("message", JSON.stringify(players));
 
   socket.on("message", (message) => {
     try {
